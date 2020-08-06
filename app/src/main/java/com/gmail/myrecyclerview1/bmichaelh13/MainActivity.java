f89b3fb8807a744e812c516d7379aeb181355b58
HarrisMBrad <bmichaelh13@gmail.com>
2020-08-06 09:15:31 -0400

    Added listener to each item on list...-BH

--- app/src/main/java/com/gmail/myrecyclerview1/bmichaelh13/MainActivity.java
+++ app/src/main/java/com/gmail/myrecyclerview1/bmichaelh13/MainActivity.java
@@ -1,15 +1,7 @@
 package com.gmail.myrecyclerview1.bmichaelh13;
 
-
-import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
 
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,19 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import Adapter.MyAdapter;
 import Model.ListItem;
 
 
-public class MainActivity extends AppCompatActivity  implements Adapter {
-
-
+public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<ListItem> listItems;
-
-    public MainActivity(RecyclerView recyclerView) {
-        this.recyclerView = recyclerView;
-    }
 
 
     @Override
@@ -37,110 +24,35 @@ public class MainActivity extends AppCompatActivity  implements Adapter {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewId);
+        recyclerView = findViewById(R.id.recyclerViewId);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         listItems = new ArrayList<>();
 
+        //ListItem item1 = new ListItem("One Star", "Movie 1", "A one hour satire that lands flat");
+        //ListItem item2 = new ListItem("Five Star", "Movie 2", "A two hour tour de force");
+
         for (int i = 0; i < 10; i++) {
-            ListItem item = new ListItem("Item" + (i + 1),
-                    "Description"
+            ListItem item = new ListItem("Rating",
+                    "Item" + (i + 1), "Excellent!"
+
             );
+
             listItems.add(item);
+            //listItems.add(item2);
+
 
         }
-
         adapter = new MyAdapter(this, listItems);
         recyclerView.setAdapter(adapter);
 
-
     }
 
-    @Override
-    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
 
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
-
-    @Override
-    public int getItemViewType(int i) {
-        return 0;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Nullable
-    @Override
-    public CharSequence[] getAutofillOptions() {
-        return new CharSequence[0];
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    private static class MyAdapter extends RecyclerView.Adapter {
-        public MyAdapter(MainActivity mainActivity, List<ListItem> listItems) {
-
-
-        }
-
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-    }
 }
+
+
 
 
 
